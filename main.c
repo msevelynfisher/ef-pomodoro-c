8acc814c878ed234ad0e2bb900a445e2b0c9e94f
Evelyn Fisher <msevelynfisher@gmail.com>
2024-07-09 17:23:05 -0500

    Basic functionality implemented.

--- main.c
+++ main.c
@@ -1,15 +1,102 @@
+#include "stdio.h"
+#include "string.h"
 #include "raylib.h"
 
-int main() {
-  InitWindow(400, 300, "Pomodoro Timer");
+void seconds_to_str(char* buffer, int seconds) {
+  char* digits = "0123456789";
+
+  int mins = seconds / 60;
+  int secs = seconds % 60;
+  buffer[0] = digits[mins/10];
+  buffer[1] = digits[mins%10];
+  buffer[2] = ':';
+  buffer[3] = digits[secs/10];
+  buffer[4] = digits[secs%10];
+  buffer[5] = '\0';
+}
+
+int test() {
+  char buffer[16];
+  char* status;
+
+  seconds_to_str(buffer, 60 * 3 + 11);
+  status = (strcmp(buffer, "03:11") == 0) ? "PASS" : "FAIL";
+  printf("[%s] Expected '03:11'. Got '%s'.\n", status, buffer);
+}
+
+#define FRAMES_WORK (25*60*60)
+#define FRAMES_BREAK (5*60*60)
+
+#define STATE_WORK_PAUSED  0
+#define STATE_WORK         1
+#define STATE_BREAK_PAUSED 2
+#define STATE_BREAK        3
+
+int app() {
+  InitWindow(400, 80, "Pomodoro Timer");
   SetTargetFPS(60);
 
+  int state = STATE_WORK_PAUSED;
+  int frames_remaining = FRAMES_WORK;
+  char buffer[16];
+
+  int frame_count = 0;
   while (!WindowShouldClose()) {
+    frame_count += 1;
+
     BeginDrawing();
     ClearBackground(BLACK);
+
+    seconds_to_str(buffer, frames_remaining/60);
+
+    if (state == STATE_WORK_PAUSED) {
+      if (frame_count % 60 < 30) {
+        DrawText(buffer, 15, 15, 50, RED);
+      }
+
+      if (IsKeyPressed(KEY_SPACE)) state = STATE_WORK;
+    } else if (state == STATE_WORK) {
+      DrawText(buffer, 15, 15, 50, RED);
+
+      if (frames_remaining > 0) frames_remaining -= 1;
+
+      if (IsKeyPressed(KEY_SPACE)) {
+        state = STATE_WORK_PAUSED;
+      }
+
+      if (frames_remaining == 0) {
+        state = STATE_BREAK_PAUSED;
+        frames_remaining = FRAMES_BREAK;
+      }
+    } else if (state == STATE_BREAK_PAUSED) {
+      if (frame_count % 60 < 30) {
+        DrawText(buffer, 15, 15, 50, BLUE);
+      }
+
+      if (IsKeyPressed(KEY_SPACE)) state = STATE_BREAK;
+    } else /* state == STATE_BREAK */ {
+      DrawText(buffer, 15, 15, 50, BLUE);
+      
+      if (frames_remaining > 0) frames_remaining -= 1;
+
+      if (IsKeyPressed(KEY_SPACE)) {
+        state = STATE_BREAK_PAUSED;
+      }
+
+      if (frames_remaining == 0) {
+        state = STATE_WORK_PAUSED;
+        frames_remaining = FRAMES_WORK;
+      }
+    }
 
     EndDrawing();
   }
 
   CloseWindow();
+
+  return 0;
+}
+
+int main() {
+  return app();
 }
